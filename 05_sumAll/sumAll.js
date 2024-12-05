4a22c48fa653b1d06081e076e23c2761a2cd4ca8
Oliver-asm <110045756+Oliver-asm@users.noreply.github.com>
2024-12-05 13:10:05 -0400

    complete: Task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(firstValue, secondValue) {
+    let suma=0;
+    if(typeof firstValue !== typeof secondValue || firstValue < 0
+        || firstValue % 1 !== 0
+    ) return "ERROR";
+    else if(firstValue < secondValue) {
+        for(; firstValue <= secondValue; firstValue++){
+            suma+=firstValue;
+        }
+        return suma;
+    } else if(firstValue > secondValue) {
+        for(; secondValue <= firstValue; secondValue++){
+            suma+=secondValue;
+        }
+        return suma;
+    }
 };
 
 // Do not edit below this line
